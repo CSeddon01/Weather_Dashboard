3d8bc75d56e86f15e036f30e797194f6078b652a
Christy Seddon <89155206+CSeddon01@users.noreply.github.com>
2022-01-31 15:44:20 -0600

    Added apiKey and weather display

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,26 @@
-console.log("say hi");
+let weather = {
+  "apiKey": "c369f155016b5438a2e3977424bf7b67",
+  fetchWeather: function(city) {
+      fetch(
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&units=metric&appid=" +
+        this.apiKey
+    )
+      .then((response) => response.json())
+      .then((data) => this.displayWeather(data));
+  },
+
+  displayWeather: function(data) {
+    const { name } = data;
+    const { icon } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    console.log(name, icon, temp, humidity, speed);
+    document.querySelector(".city").innerText = "Weather in " + name;
+    document.querySelector(".icon").src ="http://openweathermap.org/img/wn/" + icon + ".png";
+    document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+    document.querySelector(".wind").innerText = "Wind speed: " + speed + " km/h";
+    document.querySelector(".temp").innerText = temp + "°C";
+  }
+}

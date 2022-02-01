9d0c4a76f20f7077500b5931baa783b6e763b757
Christy Seddon <89155206+CSeddon01@users.noreply.github.com>
2022-02-01 14:20:10 -0600

    added icon and current date

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,9 @@
+
+// Saving the current date
+let today = new Date().toLocaleDateString();
+console.log(today);
+
+// API key from openweathermap.org
 let weather = {
   "apiKey": "c369f155016b5438a2e3977424bf7b67",
   fetchWeather: function(city) {
@@ -10,35 +16,41 @@ let weather = {
       .then((response) => response.json())
       .then((data) => this.displayWeather(data));
   },
-
+ 
+  // Retrieve items from weather service and attaches them to the proper class
   displayWeather: function(data) {
-
     const { name } = data;
     const { icon } = data.weather[0];
     const { temp, humidity } = data.main;
     const { speed } = data.wind;
     console.log(name, icon, temp, humidity, speed);
+    
+    document.querySelector(".currentDay").innerText = " " + today + " ";
     document.querySelector(".city").innerText = name;
-    document.querySelector(".icon").src ="http://openweathermap.org/img/wn/" + icon + ".png";
+    document.querySelector(".icon").src ="https://openweathermap.org/img/wn/" + icon + ".png";
     document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
     document.querySelector(".wind").innerText = "Wind: " + speed + " MPH";
     document.querySelector(".temp").innerText = "Temp: " + temp + "°F";
+   
   },
+  // Links the function to the search button
   search: function () {
     this.fetchWeather(document.querySelector(".search-bar").value);
   },
 }
 
+// Event listener for the search button click
 document.querySelector(".search button").addEventListener("click", function () {
   weather.search();
 });
 
-// document
-//   .querySelector(".search-bar")
-//   .addEventListener("keyup", function (event) {
-//     if (event.key == "Enter") {
-//       weather.search();
-//     }
-//   });
+// Added an enter button click event listener
+document
+  .querySelector(".search-bar")
+  .addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+      weather.search();
+    }
+  });
 
 // weather.fetchWeather();

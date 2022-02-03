ce98309c36130eceed9dc7ae6c0ca7502801beb6
Christy Seddon <89155206+CSeddon01@users.noreply.github.com>
2022-02-02 18:19:04 -0600

    Added lat and long searches

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,20 +23,36 @@ let weather = {
     const { icon } = data.weather[0];
     const { temp, humidity } = data.main;
     const { speed } = data.wind;
-    console.log(name, icon, temp, humidity, speed);
-    
+    const  lat  = data.coord.lat;
+    const  lon  = data.coord.lon;
+    const { uvIndex } = data.current.uvi;
+    console.log(name, icon, temp, humidity, speed, lat, lon, uvIndex);
+    var latLonPair = lat.toString() + " " + lon.toString();
+    localStorage.setItem(name, latLonPair);
     document.querySelector(".currentDay").innerText = " " + today + " ";
     document.querySelector(".city").innerText = name;
     document.querySelector(".icon").src ="https://openweathermap.org/img/wn/" + icon + ".png";
     document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
     document.querySelector(".wind").innerText = "Wind: " + speed + " MPH";
     document.querySelector(".temp").innerText = "Temp: " + temp + "°F";
-   
+    document.querySelector("UV Index:" + uvIndex);
   },
   // Links the function to the search button
   search: function () {
     this.fetchWeather(document.querySelector(".search-bar").value);
   },
+  latLon: function(position) {
+    const {latitude, longitude} = position.coords;
+    api = `https://api.openweathermap.org/data/2.5/weather?"
+    lat=${latitude}&lon=${longitude}&units=metric&appid=c369f155016b5438a2e3977424bf7b67`;
+    fetchData();
+  },
+  // displayUVI: function() {
+  // const  uvIndex  = data.current.uvi;
+  //   document.querySelector(".UV").innerText = "UV Index:" + uvIndex;
+  //   console.log("hi" + displayUVI);
+  // },
+  
 }
 
 // Event listener for the search button click
